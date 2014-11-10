d2108626b4fc46c83de3c54e3a65eafbce2c891d
Ryan Yonzon <rawswift@gmail.com>
2014-11-10 08:49:11 +0800

    Basic iterator methods

---
+++ basics/iterators.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+# iterates from 1 to 5
+puts ".upto method:"
+1.upto(5) { |n| puts n }
+
+# iterates from 10 down to 1
+puts ".downto method:"
+10.downto(1) { |n| puts n }
+
+# iterates from 0 to 30, by 2
+puts ".step method:"
+0.step(30, 2) { |n| puts n }
+
