d27afed8bc866a54f6493a79edeb1a99b760afa2
Ryan Yonzon <rawswift@gmail.com>
2014-11-10 12:17:40 +0800

    Minor mod on basics/iterators.rb

--- basics/iterators.rb
+++ basics/iterators.rb
@@ -2,13 +2,25 @@
 
 # iterates from 1 to 5
 puts ".upto method:"
+
 1.upto(5) { |n| puts n }
 
 # iterates from 10 down to 1
 puts ".downto method:"
+
 10.downto(1) { |n| puts n }
 
 # iterates from 0 to 30, by 2
 puts ".step method:"
+
 0.step(30, 2) { |n| puts n }
+
+# while iterator
+puts "while iterator:"
+
+n = 0
+while n <= 9
+  puts n
+  n += 1
+end
 
